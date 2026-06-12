82a6295970166c0965eb827b623690a4a14d3ed6
Tu Nombre <tuemail@ejemplo.com>
2026-06-12 16:18:18 -0500

    ADD: Ejercicio 02 - Stack Sorter en Java

--- src/utils/StackSorter.java
+++ src/utils/StackSorter.java
@@ -1,21 +1,23 @@
 package utils;
 import java.util.Stack;
 
-public class StackSorter {
+public class StackSorter { // StackSorter usa un stack usando otro s como auxiliar
     public void sortStack(Stack<Integer> stack) {
-        Stack<Integer> tempStack = new Stack<>();
+        Stack<Integer> tempStack = new Stack<>(); 
         
         while (!stack.isEmpty()) {
-            int temp = stack.pop();
+            int aux = stack.pop(); // saca el tope del stack original. guarda el elemento que se desea en el lugar correcto.
             
-            while (!tempStack.isEmpty() && tempStack.peek() > temp) {
-                stack.push(tempStack.pop());
+            while (!tempStack.isEmpty() && tempStack.peek() > aux) { 
+                // ordena de mayor a menor en el tempStack(Stack temporal), mayor queda en el tope (descendente)
+                // mueve los elementos mayores que aux de vuelta al stack original
+                stack.push(tempStack.pop()); //stack.push() agrega un elemento de la pila, en el tope. regresa el elemento
             }
             
-            tempStack.push(temp);
+            tempStack.push(aux); //inserta aux en tempstack. aux queda en la posición correcta.
         }
         
-        while (!tempStack.isEmpty()) {
+        while (!tempStack.isEmpty()) { // pasa todo devuelta al stack original
             stack.push(tempStack.pop());
         }
     }
